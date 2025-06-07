d2b0b8571350c18dbe14803f33628457bd106d7c
kir <kirpro95@yandex.ru>
2025-06-07 10:54:18 +0300

    initial commit

---
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_cource_springboot/SpringCourceSpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.zaurtregulov.spring.springboot.spring_cource_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourceSpringbootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringCourceSpringbootApplication.class, args);
+
+	}
+
+}
